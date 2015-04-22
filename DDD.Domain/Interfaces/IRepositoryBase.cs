18b80c2d7deab6efe5ea33b59af39699dffb21d5
Lucas Costa <lucasvbc@live.com>
2015-04-22 10:42:46 -0300

    Criando as interfaces de repositorio.

---
+++ DDD.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DDD.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+        IEnumerable<TEntity> GetList();
+        TEntity GetById(Guid id);
+        void Dispose();
+    }
+}
