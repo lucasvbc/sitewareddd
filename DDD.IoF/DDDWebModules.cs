6cc13f7de342fe7dd29f1a7ae7b337526cdb5b6b
Lucas Costa <lucasvbc@live.com>
2015-04-22 11:08:04 -0300

    Camada que armazena o modulo do injetor de dependencia, para que na aplicação de interface não tenhamos que ter a referencia a camada de dados. Utilizado o ninject

---
+++ DDD.IoF/DDDWebModules.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ninject.Modules;
+using DDD.Application.Interface;
+using DDD.Application;
+using DDD.Domain.Interfaces.Services;
+using DDD.Domain.Services;
+using DDD.Domain.Interfaces.Repositories;
+using DDD.Data.Repositories;
+
+namespace DDD.IoC
+{
+    public class DDDWebModules : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind(typeof(IAppServiceBase<>)).To(typeof(AppServiceBase<>));
+            Bind<IProductAppService>().To<ProductAppService>();
+            Bind<IOfferAppService>().To<OfferAppService>();
+
+            Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+            Bind<IProductService>().To<ProductService>();
+            Bind<IOfferService>().To<OfferService>();
+
+            Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
+            Bind<IProductRepository>().To<ProductRepository>();
+            Bind<IOfferRepository>().To<OfferRepository>();
+        }
+    }
+}
